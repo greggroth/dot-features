7d4f4337fc38d633bb02e35e3cec3f9cb8952dce
Greggory Rothmeier <greggroth@github.com>
2022-05-19 15:31:09 -0700

    Add zsh feature

---
+++ src/zsh/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+VERSION=${1:-"latest"}
+USERNAME=${2:-"automatic"}
+
+# If in automatic mode, determine if a user already exists, if not use vscode
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=vscode
+    fi
+elif [ "${USERNAME}" = "none" ]; then
+    USERNAME=root
+    USER_UID=0
+    USER_GID=0
+fi
+
+echo "Changing default shell for $USERNAME to $(which zsh)"
+sudo chsh -s $(which zsh) $USERNAME
